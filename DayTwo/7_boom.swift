c04a492320f1bca41b75431b8e7bcebb87e0cc6c
deborrrrrah <dajtpbl@gmail.com>
2021-12-21 13:21:18 +0700

    [Day-2] 7 Boom refactored

--- DayTwo/7_boom.swift
+++ DayTwo/7_boom.swift
@@ -3,8 +3,20 @@ let userInput = readLine()
 let numberInput = userInput!
 let myNum = Int(numberInput) ?? 0
 
+func isFactorOf(num: Int, factor: Int) -> Bool {
+  return num % factor == 0
+}
+
+func isContainsDigit(num: Int, digit: Int) -> Bool {
+  return num % 10 == digit || num / 10 == digit
+}
+
+func isContainsChar(string: String, char: Character) -> Bool {
+  return string.contains(char)
+}
+
 print("Approach 1")
-if myNum % 7 == 0 || myNum % 10 == 7 || myNum / 10 == 7 {
+if isFactorOf(num: myNum, factor: 7) || isContainsDigit(num: myNum, digit: 7) {
   print("Boom")
 } else {
   print("Safe number")
@@ -13,7 +25,7 @@ if myNum % 7 == 0 || myNum % 10 == 7 || myNum / 10 == 7 {
 print()
 
 print("Approach 2")
-if myNum % 7 == 0 || numberInput.contains("7"){
+if isFactorOf(num: myNum, factor: 7) || isContainsChar(string: numberInput, char: "7") {
   print("Boom")
 } else {
   print("Safe number")
