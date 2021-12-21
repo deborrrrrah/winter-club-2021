29161a9ece264f0b929f043e2987b95f1462d374
deborrrrrah <dajtpbl@gmail.com>
2021-12-21 21:54:29 +0700

    [Day-3] Refactor N-size Square

--- DayThree/n_size_square.swift
+++ DayThree/n_size_square.swift
@@ -3,12 +3,15 @@ let userInput = readLine()
 let numberInput = userInput!
 let size = Int(numberInput) ?? 0
 
+let firstIndex: Int = 0
+let lastIndex: Int = size - 1
+
 // Approach 1
 print("Approach 1")
-for row in stride(from: 1, to: size + 1, by: 1) {
+for row in stride(from: firstIndex, to: size, by: 1) {
   var rowContent: String = ""
-  for col in stride(from: 1, to: size + 1, by: 1) {
-    if row == 1 || row == size || col == 1 || col == size {
+  for col in stride(from: firstIndex, to: size, by: 1) {
+    if row == firstIndex || row == lastIndex || col == firstIndex || col == lastIndex {
       rowContent += "#"
     } else {
       rowContent += " "
@@ -19,9 +22,9 @@ for row in stride(from: 1, to: size + 1, by: 1) {
 
 // Approach 2
 print("\nApproach 2")
-for row in stride(from: 1, to: size + 1, by: 1) {
-  for col in stride(from: 1, to: size + 1, by: 1) {
-    if row == 1 || row == size || col == 1 || col == size {
+for row in stride(from: firstIndex, to: size, by: 1) {
+  for col in stride(from: firstIndex, to: size, by: 1) {
+    if row == firstIndex || row == lastIndex || col == firstIndex || col == lastIndex {
       print("#", terminator: "")
     } else {
       print(" ", terminator: "")
